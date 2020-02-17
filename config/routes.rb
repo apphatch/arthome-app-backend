63553418a2c477854408e64df4809c2ac07ef476
Klinkesorn Nguyen An Hoa <k.nguyen.an.hoa@gmail.com>
2020-02-17 15:42:10 +0700

    Turn on some security features.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'home#index'
+  resource :login do
+  end
 
-  resource :user do
+  resource :session do
+  end
+
+  resources :users do
   end
 
   resources :checklists do
