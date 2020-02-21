400bcaa8768aa58fc12316039a913d51c700adb7
Klinkesorn Nguyen An Hoa <k.nguyen.an.hoa@gmail.com>
2020-02-21 21:20:41 +0700

    Logout should be a get request.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   post 'login', to: 'sessions#create', as: :login
-  post 'logout', to: 'sessions#destroy', as: :logout
+  get 'logout', to: 'sessions#destroy', as: :logout
 
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
