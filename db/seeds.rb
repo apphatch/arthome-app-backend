933ecfff5a35b8c23767342aa24d491f1962b284
Klinkesorn Nguyen An Hoa <k.nguyen.an.hoa@gmail.com>
2020-02-26 13:00:05 +0700

    Add db seeding information.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.create name: 'Default User', username: 'default_user', password: 'password'
+shop = Shop.create name: 'Shop 1', shop_type: 'IP', full_address: '123 Nguyen Van Luong', city: 'HCM', district: 'Go Vap'
+
+user.shops.push shop
+
+#auto-gen when user - shop relation established
+checklist = user.checklists.first
+checklist.reference = '12345'
+
+stock = Stock.create name: 'Stock 1', sku: '123ABC', barcode: '1234567891011', category: 'Testers', group: 'Group 1', role: 'Tester', packaging: 'Paperbag', role_shop: 'IP'
+shop.stocks.push stock
+checklist.stocks.push stock
