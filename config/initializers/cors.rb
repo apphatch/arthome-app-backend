b78adcd55d2975f034d90ebb127065c82ecd89b1
Klinkesorn Nguyen An Hoa <k.nguyen.an.hoa@gmail.com>
2020-07-10 20:36:40 +0700

    FIX: limit CORS domain to localhost for now

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,9 +1,10 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'http://localhost:3100'
     resource '*',
       headers: :any,
-      methods: [:get, :post, :patch, :put],
+      methods: [:get, :post, :patch, :put, :delete, :options, :head],
+      credentials: true,
       expose: ['X-CSRF-Token']
   end
 end
