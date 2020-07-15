f179e4e3eddf852c17667b9b9fdf0bc897e41a13
Klinkesorn Nguyen An Hoa <k.nguyen.an.hoa@gmail.com>
2020-07-15 22:04:09 +0700

    Add stock category to checklist item serializer

--- app/serializers/checklist_item_serializer.rb
+++ app/serializers/checklist_item_serializer.rb
@@ -1,7 +1,11 @@
 class ChecklistItemSerializer < ActiveModel::Serializer
-  attributes :id, :data, :stock_name
+  attributes :id, :data, :stock_name, :category
 
   def stock_name
     object.stock.name
+  end
+
+  def category
+    object.stock.try(:category)
   end
 end
