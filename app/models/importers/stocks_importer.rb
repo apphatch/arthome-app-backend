a0b3fa3a6f4927b900932a6296d54184835e5a34
Klinkesorn Nguyen An Hoa <k.nguyen.an.hoa@gmail.com>
2020-08-29 10:44:11 +0700

    FIX: downcase the role attribute when importing

--- app/models/importers/stocks_importer.rb
+++ app/models/importers/stocks_importer.rb
@@ -27,6 +27,7 @@ module Importers
         unless assocs[:shops].nil?
           assocs[:shops] = Shop.find_by_importing_id assocs[:shops]
         end
+        attributes[:role] = attributes[:role].downcase
         attributes[:custom_attributes] = {
           category: attributes[:category],
           sub_category: attributes[:sub_category],
