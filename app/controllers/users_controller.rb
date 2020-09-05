fb9f439934f880c6ac62540b67459dc809f9b237
Klinkesorn Nguyen An Hoa <k.nguyen.an.hoa@gmail.com>
2020-09-05 13:33:57 +0700

    FIX: using base64 files now so not returning io stream anymore

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -60,8 +60,8 @@ class UsersController < ApplicationController
   end
 
   def import_template
-    data = Importers::OsaUsersImporter.template.string if @current_app == 'osa-webportal'
-    data = Importers::QcUsersImporter.template.string if @current_app == 'qc'
+    data = Importers::OsaUsersImporter.template if @current_app == 'osa-webportal'
+    data = Importers::QcUsersImporter.template if @current_app == 'qc'
     f = File.open 'export/user-import-template.xls', 'rb'
     enc = Base64.encode64 f.read
     send_data enc, type: :xls, filename: 'user-import-template.xls'
