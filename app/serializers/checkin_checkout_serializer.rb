8caff9a37983988c5a949158d6c74c98928d033b
Klinkesorn Nguyen An Hoa <k.nguyen.an.hoa@gmail.com>
2020-09-12 14:53:51 +0700

    If user_checkout not present, serialize a null data template

--- app/serializers/checkin_checkout_serializer.rb
+++ app/serializers/checkin_checkout_serializer.rb
@@ -1,9 +1,14 @@
 class CheckinCheckoutSerializer < ActiveModel::Serializer
-  attributes :id, :time, :note, :is_checkin, :user_checkout, :shop_checkouts
+  attributes :id, :time, :note, :is_checkin, :user_checkout, :shop_checkouts,
+    :shop_checkout_photos
 
   has_one :user
   has_one :shop
   has_many :photos, each_serializer: PhotoSerializer
   has_many :checkouts, each_serializer: CheckinCheckoutSerializer
   belongs_to :checkin, serializer: CheckinCheckoutSerializer
+
+  def user_checkout
+    super || CheckinCheckout.new.attributes
+  end
 end
