422259b820a9fe78188abb2137dcb34d501199ab
Klinkesorn Nguyen An Hoa <k.nguyen.an.hoa@gmail.com>
2020-09-12 19:26:30 +0700

    Add more importers to osa webportal factory

--- app/models/apps/osa_webportal_factory.rb
+++ app/models/apps/osa_webportal_factory.rb
@@ -4,6 +4,9 @@ module Apps
       use Importers::OsaUsersImporter, as: :user_importer
       use Importers::OsaStocksImporter, as: :stock_importer
       use Importers::OsaShopsImporter, as: :shop_importer
+      use Importers::ChecklistsImporter, as: :checklist_importer
+      use Importers::ChecklistItemsImporter, as: :checklist_item_importer
+      use Importers::OsaMasterImporter, as: :master_importer
     end
   end
 end
