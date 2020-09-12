598330aa8b04b680f0473564a337b0961c508a7c
Klinkesorn Nguyen An Hoa <k.nguyen.an.hoa@gmail.com>
2020-09-12 15:02:27 +0700

    FIX: remove undefined method

--- app/serializers/checkin_checkout_serializer.rb
+++ app/serializers/checkin_checkout_serializer.rb
@@ -1,6 +1,5 @@
 class CheckinCheckoutSerializer < ActiveModel::Serializer
-  attributes :id, :time, :note, :is_checkin, :user_checkout, :shop_checkouts,
-    :shop_checkout_photos
+  attributes :id, :time, :note, :is_checkin, :user_checkout, :shop_checkouts
 
   has_one :user
   has_one :shop
