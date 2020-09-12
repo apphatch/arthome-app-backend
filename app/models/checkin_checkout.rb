45a492d0de0d6faf789563c90a827f8751dfa1a3
Klinkesorn Nguyen An Hoa <k.nguyen.an.hoa@gmail.com>
2020-09-12 11:23:10 +0700

    Add helper functions for checkin checkouts

--- app/models/checkin_checkout.rb
+++ app/models/checkin_checkout.rb
@@ -8,4 +8,12 @@ class CheckinCheckout < ApplicationRecord
 
   scope :user, -> { where.not(user: nil) }
   scope :shop, -> { where(user: nil) }
+
+  def checkin?
+    return self.is_checkin
+  end
+
+  def checkout?
+    return !self.is_checkin
+  end
 end
