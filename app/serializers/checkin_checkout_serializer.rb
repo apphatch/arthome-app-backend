e349aa3a0bb4e5989e6af595616e0a8aca970c34
Klinkesorn Nguyen An Hoa <k.nguyen.an.hoa@gmail.com>
2020-09-14 22:00:39 +0700

    Add serializer for shop checkout photos only

--- app/serializers/checkin_checkout_serializer.rb
+++ app/serializers/checkin_checkout_serializer.rb
@@ -1,5 +1,6 @@
 class CheckinCheckoutSerializer < ActiveModel::Serializer
-  attributes :id, :time, :note, :is_checkin, :user_checkout, :shop_checkouts
+  attributes :id, :time, :note, :is_checkin,
+    :user_checkout, :shop_checkouts, :shop_checkout_photos
 
   has_one :user
   has_one :shop
@@ -13,5 +14,11 @@ class CheckinCheckoutSerializer < ActiveModel::Serializer
 
   def shop_checkouts
     object.shop_checkouts.present? ? object.shop_checkouts : [CheckinCheckout.new.attributes]
+  end
+
+  def shop_checkout_photos
+    if object.shop_checkouts.present?
+      return object.shop_checkouts.collect{|c| c.photos}.flatten
+    end
   end
 end
