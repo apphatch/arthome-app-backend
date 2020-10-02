5f8ffa1af30e7c746c5849ea7d481f044f289df0
Klinkesorn Nguyen An Hoa <k.nguyen.an.hoa@gmail.com>
2020-10-02 17:27:06 +0700

    Serialize photos for user_checkout records

--- app/serializers/checkin_checkout_serializer.rb
+++ app/serializers/checkin_checkout_serializer.rb
@@ -1,17 +1,29 @@
 class CheckinCheckoutSerializer < ActiveModel::Serializer
   attributes :id, :time, :note, :is_checkin,
-    :user_checkout, :shop_checkouts, :shop_checkout_photos, :created_at
+    :user_checkout, :shop_checkouts, :shop_checkout_photos,
+    :created_at
 
   has_one :user
   #TODO fix this so it takes a proper serializer
   has_one :shop, serializer: nil
+
   has_many :photos, each_serializer: PhotoSerializer
   has_many :shop_checkout_photos, each_serializer: PhotoSerializer
   has_many :checkouts, each_serializer: CheckinCheckoutSerializer
   belongs_to :checkin, serializer: CheckinCheckoutSerializer
 
+  #may need to refac
   def user_checkout
-    object.user_checkout.present? ? object.user_checkout : CheckinCheckout.new.attributes
+    cico = CheckinCheckout.new.attributes
+    if object.user_checkout.present?
+      photo = object.user_checkout.photos.first
+      photo = PhotoSerializer.new(photo).serializable_hash if photo.present?
+      cico = object.user_checkout.serializable_hash.merge(photo: photo)
+    end
+  end
+
+  def user_checkout_photo
+    object.user_checkout.photos.first
   end
 
   def shop_checkouts
