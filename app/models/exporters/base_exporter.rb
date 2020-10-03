3717eb40b5eaf10d00b369a331f04d9389564ddf
Klinkesorn Nguyen An Hoa <k.nguyen.an.hoa@gmail.com>
2020-10-03 22:25:51 +0700

    REFAC: base exporter handles nested data

--- app/models/exporters/base_exporter.rb
+++ app/models/exporters/base_exporter.rb
@@ -5,6 +5,7 @@ module Exporters
     def initialize params={}
       @output_file = params[:output]
       @params = params
+      @max_flatten_level = 1
     end
 
     def set_headers list
@@ -15,6 +16,21 @@ module Exporters
       @data = list
     end
 
+    def set_flatten_level level
+      @max_flatten_level = level
+    end
+
+    def flatten_and_apply method, data, curr_level=0, curr_index=0
+      if curr_level < @max_flatten_level
+        data.each do |d|
+          curr_index = flatten_and_apply method, data, curr_level + 1, curr_index
+        end
+      else
+        method.call data, curr_index
+      end
+      return curr_index + 1
+    end
+
     def export
       raise Exception.new 'provide headers as array' if @headers.empty?
       raise Exception.new 'provide data as array' unless @data.is_a?(Array)
@@ -23,9 +39,11 @@ module Exporters
       book.create_worksheet
 
       book.worksheet(0).insert_row(0, @headers)
-      @data.each_with_index do |row, index|
+
+      insert_into_spreadsheet = -> (row, index) {
         book.worksheet(0).insert_row(index+1, row)
-      end
+      }
+      flatten_and_apply insert_into_spreadsheet, @data
 
       book.write @output_file
     end
--- app/models/exporters/qc_exporter.rb
+++ app/models/exporters/qc_exporter.rb
@@ -7,29 +7,18 @@ module Exporters
     end
 
     def export
-      @headers = [
+      set_headers [
         'Time', 'Store Type (MT/DT/CVS)', 'City', 'NPP', 'Tên Cửa Hàng',
         'Địa Chỉ', 'Audit', 'U/C', 'Package', 'Category', 'Product Group',
         'SKU Name', 'SKU', 'NSX or HSD', 'Lỗi', 'Green', 'Yellow', 'Red', 'Image'
       ]
-      @data = Mappers::QcExportMapper.map ChecklistItem.active.filter{ |c|
+      set_data Mappers::QcExportMapper.map ChecklistItem.active.filter{ |c|
         c.checklist.checklist_type == 'qc' &&
           date_filter(c, @params)
       }
+      set_flatten_level 2
 
-      book = Spreadsheet::Workbook.new
-      book.create_worksheet
-
-      book.worksheet(0).insert_row(0, @headers)
-      rowcount = 0
-      @data.each do |row|
-        row.each do |subrow|
-          rowcount += 1
-          book.worksheet(0).insert_row(rowcount, subrow)
-        end
-      end
-
-      book.write @output_file
+      super
     end
   end
 end
