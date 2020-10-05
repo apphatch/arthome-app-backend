278cd14e4a52dc4a29ff402ff177f9774c973f68
Klinkesorn Nguyen An Hoa <k.nguyen.an.hoa@gmail.com>
2020-10-05 22:43:10 +0700

    FIX: reports should instantiate before generating

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,11 +1,11 @@
 class ReportsController < ApplicationController
   def qc_summary
-    report = @current_app.get(:summary_report)
+    report = @current_app.get(:summary_report).new
     render json: report.generate
   end
 
   def qc_detail
-    report = @current_app.get(:detail_report)
+    report = @current_app.get(:detail_report).new
     render json: report.generate
   end
 end
