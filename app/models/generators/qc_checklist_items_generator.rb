62262b68221c06317b834f1e2414cc5dbdc23784
Klinkesorn Nguyen An Hoa <k.nguyen.an.hoa@gmail.com>
2020-10-17 14:37:49 +0700

    FIX: qc checklist item generator to include app group

--- app/models/generators/qc_checklist_items_generator.rb
+++ app/models/generators/qc_checklist_items_generator.rb
@@ -2,27 +2,28 @@ module Generators
   class QcChecklistItemsGenerator < BaseGenerator
     def initialize params={}
       @checklists = params[:checklists]
+      @app_group = 'qc'
       super params
     end
 
     def generate_hpc checklist
       stock = Stock.active.where role: 'hpc'
       stock.each do |st|
-        checklist.checklist_items.create stock: st
+        checklist.checklist_items.create stock: st, app_group: @app_group
       end
     end
 
     def generate_ic checklist
       stock = Stock.active.where role: 'ic'
       stock.each do |st|
-        checklist.checklist_items.create stock: st
+        checklist.checklist_items.create stock: st, app_group: @app_group
       end
     end
 
     def generate_hpcic checklist
       stock = Stock.active.where role: ['hpc', 'ic']
       stock.each do |st|
-        checklist.checklist_items.create stock: st
+        checklist.checklist_items.create stock: st, app_group: @app_group
       end
     end
 
