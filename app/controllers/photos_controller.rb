a3b69337bd95cc1e4cc22404762953a873031b2f
Klinkesorn Nguyen An Hoa <k.nguyen.an.hoa@gmail.com>
2020-10-23 14:03:41 +0700

    Update photo create to use app_group

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,9 +1,10 @@
 class PhotosController < ApplicationController
   def create
     photo = Photo.create(
-      image: params[:photo],
+      image: params[:photo_uri],
       time: params[:time],
-      name: params[:name]
+      name: params[:name],
+      app_group: params[:app_group]
     )
     render json: photo.image_path
   end
