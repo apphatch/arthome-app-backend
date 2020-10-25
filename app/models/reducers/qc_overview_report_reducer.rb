6bc02e829e41dce80c8f6f64be432e5f8bc19c3d
Klinkesorn Nguyen An Hoa <k.nguyen.an.hoa@gmail.com>
2020-10-25 20:22:16 +0700

    Update qc detail report logic

--- app/models/reducers/qc_overview_report_reducer.rb
+++ app/models/reducers/qc_overview_report_reducer.rb
@@ -2,14 +2,16 @@ module Reducers
   class QcOverviewReportReducer < BaseReducer
     def self.apply_each checklist_item
       stats = [0, 0, 0]
+      record_length = 0
       if checklist_item.data.present?
         records = checklist_item.data["records"]
+        record_length = records.length
         stats = records.collect{|r| r["Mức cảnh báo"]}.compact
         stats = [stats.count("Xanh"), stats.count("Vàng"), stats.count("Đỏ")]
       end
       return [
-        checklist_item.stock.role == 'hpc' ? 1 : 0,
-        checklist_item.stock.role == 'ic' ? 1 : 0,
+        checklist_item.stock.role == 'hpc' ? record_length : 0,
+        checklist_item.stock.role == 'ic' ? record_length : 0,
       ] + stats
     end
   end
