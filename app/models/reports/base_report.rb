3f83d618badc24d73522a59600a7914e935789b2
Klinkesorn Nguyen An Hoa <k.nguyen.an.hoa@gmail.com>
2020-10-27 13:53:47 +0700

    FIX: dates passed from FE are not nil but are undefined (string)

--- app/models/reports/base_report.rb
+++ app/models/reports/base_report.rb
@@ -3,8 +3,8 @@ module Reports
     def initialize params={}
       @headers = []
       @data = []
-      @date_from = params[:date_from].present? ? DateTime.parse(params[:date_from]) : DateTime.now.beginning_of_day
-      @date_to = params[:date_to].present? ? DateTime.parse(params[:date_to]) : DateTime.now.end_of_day
+      @date_from = params[:date_from] != 'undefined' ? DateTime.parse(params[:date_from]) : DateTime.now.beginning_of_day
+      @date_to = params[:date_to] != 'undefined' ? DateTime.parse(params[:date_to]) : DateTime.now.end_of_day
     end
 
     def set_headers headers
