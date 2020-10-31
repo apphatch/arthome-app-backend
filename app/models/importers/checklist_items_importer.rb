543dbbbb484510c8753fa9eefcf4352d3d8091d6
Klinkesorn Nguyen An Hoa <k.nguyen.an.hoa@gmail.com>
2020-10-31 13:02:12 +0700

    Update sos checklist item importer to work with sos stocks

--- app/models/importers/checklist_items_importer.rb
+++ app/models/importers/checklist_items_importer.rb
@@ -21,7 +21,11 @@ module Importers
       index :photo_ref, ['Photo ID']
       index :shop, ['Outlet', 'Outlet Name'], as: :string
       index :user, ['OSA Code'], as: :string
-      associate :stock, ['ULV code', 'Rental ID', 'Category'], as: :string
+      associate :stock, ['ULV code', 'Rental ID', 'Category'], allow_dup: true, as: :string
+
+      index_temp :checklist_type, ['Type']
+      index_temp :stock_cat, ['Category'], allow_dup: true, as: :string
+      index_temp :stock_sub_cat, ['Sub Category'], allow_dup: true, as: :string
 
       super do |attributes, assocs, row|
         date = DateTime.parse attributes[:date] if attributes[:date].present?
@@ -35,7 +39,12 @@ module Importers
         ].join()
 
         assocs[:checklist] = Checklist.find_by_reference checklist_ref
-        assocs[:stock] = Stock.find_by_importing_id assocs[:stock]
+        if attributes[:checklist_type] == 'sos'
+          stock_importing_id = attributes[:stock_cat] + attributes[:stock_sub_cat]
+        else
+          stock_importing_id = assocs[:stock]
+        end
+        assocs[:stock] = Stock.find_by_importing_id stock_importing_id
         assocs.delete :checklist_type #prevent no method error
 
         [attributes, assocs]
