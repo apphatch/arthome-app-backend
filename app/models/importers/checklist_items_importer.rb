618c6ced0b149f9989818a4a652b102ff6a40c6c
Klinkesorn Nguyen An Hoa <k.nguyen.an.hoa@gmail.com>
2020-10-31 14:34:02 +0700

    FIX: checklist item importer should also take into account importing_id of stock which is autogenerated for sos checklist type

--- app/models/importers/checklist_items_importer.rb
+++ app/models/importers/checklist_items_importer.rb
@@ -10,7 +10,7 @@ module Importers
       auto_gen_uid_with_attributes [
         :checklist_type,
         :yearweek, :date, :user,
-        :shop, :stock
+        :shop, :stock, :stock_sub_cat
       ]
 
       index :quantity, ['Stock'], as: :float
@@ -22,9 +22,6 @@ module Importers
       index :shop, ['Outlet', 'Outlet Name'], as: :string
       index :user, ['OSA Code'], as: :string
       associate :stock, ['ULV code', 'Rental ID', 'Category'], allow_dup: true, as: :string
-
-      index_temp :checklist_type, ['Type']
-      index_temp :stock_cat, ['Category'], allow_dup: true, as: :string
       index_temp :stock_sub_cat, ['Sub Category'], allow_dup: true, as: :string
 
       super do |attributes, assocs, row|
@@ -39,11 +36,8 @@ module Importers
         ].join()
 
         assocs[:checklist] = Checklist.find_by_reference checklist_ref
-        if attributes[:checklist_type].downcase == 'sos'
-          stock_importing_id = attributes[:stock_cat] + attributes[:stock_sub_cat]
-        else
-          stock_importing_id = assocs[:stock]
-        end
+        stock_importing_id = assocs[:stock]
+        stock_importing_id += attributes[:stock_sub_cat] if attributes[:checklist_type].downcase == 'sos'
         assocs[:stock] = Stock.find_by_importing_id stock_importing_id
         assocs.delete :checklist_type #prevent no method error
 
