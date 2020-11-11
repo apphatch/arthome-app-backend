8d10fb00e0eb944a6392dbc3f1ed4a9f3a83b623
Klinkesorn Nguyen An Hoa <k.nguyen.an.hoa@gmail.com>
2020-11-11 09:57:49 +0700

    Update date filterables: filter by created_at field. Update osa checkin checkout exporter: export data from current week by default.

--- app/models/concerns/filterable/by_date.rb
+++ app/models/concerns/filterable/by_date.rb
@@ -3,6 +3,13 @@ module Filterable::ByDate
   # this is so they easily chain with && in exporters
   # as the filter can simply be bypassed
 
+  def created_at_filter obj, params
+    if params[:date_from].present? && params[:date_to].present? && obj.respond_to?(:created_at)
+      return obj.created_at.between?(params[:date_from], params[:date_to])
+    end
+    return true
+  end
+
   def date_filter obj, params
     if params[:date_from].present? && params[:date_to].present? && obj.respond_to?(:date)
       return obj.date.between?(params[:date_from], params[:date_to])
--- app/models/exporters/osa_checkin_checkout_exporter.rb
+++ app/models/exporters/osa_checkin_checkout_exporter.rb
@@ -7,13 +7,17 @@ module Exporters
     end
 
     def export
+      #may need to add timezone support
+      @params[:date_from] = DateTime.now.beginning_of_week if @params[:date_from].nil?
+      @params[:date_to] = DateTime.now.end_of_week if @params[:date_to].nil?
+
       set_headers [
         'Outlet', 'Outlet Name', 'User ID', 'User Name',
         'Note', 'Date', 'Long', 'Lat', 'Checkin/checkout'
       ]
       set_data Mappers::OsaCheckinCheckoutExportMapper.map(CheckinCheckout.active.filter{ |c|
         c.app_group == 'osa' &&
-          date_filter(c, @params) && yearweek_filter(c, @params)
+          created_at_filter(c, @params)
       }).compact
 
       super
