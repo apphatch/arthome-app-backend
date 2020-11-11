1889f3e3b14a35081adcffb1bfa7f22de9ff1cc0
Klinkesorn Nguyen An Hoa <k.nguyen.an.hoa@gmail.com>
2020-11-11 09:51:24 +0700

    REFAC: remove redundant time and created_at formatters. These are taken care of in concern

--- app/serializers/application_serializer.rb
+++ app/serializers/application_serializer.rb
@@ -1,10 +1,2 @@
 class ApplicationSerializer < ActiveModel::Serializer
-  def time
-    #default for now
-    object.time.in_time_zone 'Bangkok' if object.respond_to?(:time)
-  end
-
-  def created_at
-    object.created_at.in_time_zone 'Bangkok' if object.respond_to?(:time)
-  end
 end
