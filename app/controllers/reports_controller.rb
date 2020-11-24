65139603bc016513961cb469797950101e154e5b
Klinkesorn Nguyen An Hoa <k.nguyen.an.hoa@gmail.com>
2020-11-24 22:21:10 +0700

    FIX: pass params correctly to qc reports

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,15 +1,11 @@
 class ReportsController < ApplicationController
   def qc_summary
-    report = @current_app.get(:summary_report).new permitted_params
+    report = @current_app.get(:summary_report).new params
     render json: report.generate
   end
 
   def qc_detail
-    report = @current_app.get(:detail_report).new permitted_params
+    report = @current_app.get(:detail_report).new params
     render json: report.generate
-  end
-
-  def permitted_params
-    return params.permit(:date_from, :date_to)
   end
 end
