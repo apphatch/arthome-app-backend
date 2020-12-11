b3fb4194e37f1988a90b56f94076794df183d8ff
Klinkesorn Nguyen An Hoa <k.nguyen.an.hoa@gmail.com>
2020-12-11 12:31:41 +0700

    Add app group scope to all models

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,6 +2,7 @@ class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
   validates :app_group, presence: true
+  scope :with_app_group, -> (app_group) { where(app_group: app_group) }
   scope :active, -> { where(deleted: false) }
   scope :inactive, -> { where(deleted: true) }
 
