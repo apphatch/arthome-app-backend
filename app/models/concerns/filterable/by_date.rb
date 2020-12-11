bc5bfa89c88c78202a7395380ac31adc24954001
Klinkesorn Nguyen An Hoa <k.nguyen.an.hoa@gmail.com>
2020-12-11 20:47:23 +0700

    REFAC: filterable by date concern to use named params instead of arbitrary params hash

--- app/models/concerns/filterable/by_date.rb
+++ app/models/concerns/filterable/by_date.rb
@@ -3,30 +3,30 @@ module Filterable::ByDate
   # this is so they easily chain with && in exporters
   # as the filter can simply be bypassed
 
-  def updated_at_filter obj, params
-    if params[:date_from].present? && params[:date_to].present? && obj.respond_to?(:updated_at)
-      return obj.updated_at.between?(params[:date_from], params[:date_to])
+  def updated_at_filter obj, date_from: nil, date_to: nil
+    if date_from.present? && date_to.present? && obj.respond_to?(:updated_at)
+      return obj.updated_at.between?(date_from, date_to)
     end
     return true
   end
 
-  def created_at_filter obj, params
-    if params[:date_from].present? && params[:date_to].present? && obj.respond_to?(:created_at)
-      return obj.created_at.between?(params[:date_from], params[:date_to])
+  def created_at_filter obj, date_from: nil, date_to: nil
+    if date_from.present? && date_to.present? && obj.respond_to?(:created_at)
+      return obj.created_at.between?(date_from, date_to)
     end
     return true
   end
 
-  def date_filter obj, params
-    if params[:date_from].present? && params[:date_to].present? && obj.respond_to?(:date)
-      return obj.date.between?(params[:date_from], params[:date_to])
+  def date_filter obj, date_from: nil, date_to: nil
+    if date_from.present? && date_to.present? && obj.respond_to?(:date)
+      return obj.date.between?(date_from, date_to)
     end
     return true
   end
 
-  def yearweek_filter obj, params
-    if params[:yearweek] && obj.respond_to?(:yearweek)
-      return obj.yearweek == params[:yearweek].to_s
+  def yearweek_filter obj, yearweek: nil
+    if yearweek && obj.respond_to?(:yearweek)
+      return obj.yearweek == yearweek.to_s
     end
     return true
   end
