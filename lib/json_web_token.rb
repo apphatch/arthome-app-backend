c7b14469ce5323ef8ad2d166c75d06b9ff2beded
Klinkesorn Nguyen An Hoa <k.nguyen.an.hoa@gmail.com>
2020-12-17 22:38:24 +0700

    FIX: jwt generator should respect expiration time

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -1,11 +1,11 @@
 class JsonWebToken
   class << self
-    def encode(payload, exp = 5.minutes.from_now)
-      payload[:exp] = exp.to_i
+    def encode payload
+      payload[:exp] ||= 5.minutes.from_now
       JWT.encode(payload, Rails.application.credentials.config[:secret_key_base])
     end
 
-    def decode(token)
+    def decode token
       begin
         body = JWT.decode(token, Rails.application.credentials.config[:secret_key_base])[0]
         HashWithIndifferentAccess.new body
