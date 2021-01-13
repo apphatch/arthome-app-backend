dd02070517f184f3d3929edb00c3c832acd3fef7
Klinkesorn Nguyen An Hoa <k.nguyen.an.hoa@gmail.com>
2021-01-13 14:31:14 +0700

    FIX: time zonable concern again

--- app/serializers/concerns/time_zonable.rb
+++ app/serializers/concerns/time_zonable.rb
@@ -3,10 +3,10 @@ module TimeZonable
 
   def time
     #default for now
-    object.time.in_time_zone 'Bangkok' if object.respond_to?(:time)
+    object.time.try(:in_time_zone, 'Bangkok') if object.respond_to?(:time)
   end
 
   def created_at
-    object.created_at.in_time_zone 'Bangkok' if object.respond_to?(:created_at)
+    object.created_at.try(:in_time_zone, 'Bangkok') if object.respond_to?(:created_at)
   end
 end
