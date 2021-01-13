c186c8ee54facbbe2e177a9cd1509907991901e6
Klinkesorn Nguyen An Hoa <k.nguyen.an.hoa@gmail.com>
2021-01-13 14:16:56 +0700

    FIX: time zone conversion

--- app/serializers/concerns/time_zonable.rb
+++ app/serializers/concerns/time_zonable.rb
@@ -7,6 +7,6 @@ module TimeZonable
   end
 
   def created_at
-    object.created_at.in_time_zone 'Bangkok' if object.respond_to?(:time)
+    object.created_at.in_time_zone 'Bangkok' if object.respond_to?(:created_at)
   end
 end
