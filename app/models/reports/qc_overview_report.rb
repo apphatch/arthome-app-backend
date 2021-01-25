8f27812c400cd2fd643419e5e0a54d35ab8264f2
Klinkesorn Nguyen An Hoa <k.nguyen.an.hoa@gmail.com>
2021-01-25 11:37:51 +0700

    Update qc overview report

--- app/models/reports/qc_overview_report.rb
+++ app/models/reports/qc_overview_report.rb
@@ -10,15 +10,16 @@ module Reports
         'HPC thực', 'IC thực', 'Xanh', 'Vàng', 'Đỏ'
       ]
 
-      criteria = {date: @params[:date_from]..@params[:date_to]}
+      criteria = {updated_at: @params[:date_from]..@params[:date_to]}
       unless @date_given
-        criteria = criteria.merge(date: Time.current.beginning_of_month..Time.current.end_of_month)
+        criteria = criteria.merge(updated_at: Time.current.beginning_of_month..Time.current.end_of_month)
       end
 
       mapper = Mappers::QcOverviewReportMapper.new locale: @params[:locale]
       set_data mapper.map(
-        Checklist.active.with_app_group('qc').where(
-          criteria.merge(exclude_from_search: false)
+        Checklist.active.with_app_group('qc').includes(:checklist_items).where(
+            exclude_from_search: false,
+            checklists_items: criteria
         )
       ).compact
 
